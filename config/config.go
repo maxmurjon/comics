ea45bbe20195b313e240cb71a3121e41186df482
maxmurjon <maxmurjonkeldiyorov@gmail.com>
2024-12-06 23:36:02 +0500

    Problem solved

--- config/config.go
+++ config/config.go
@@ -47,15 +47,15 @@ func Load() *Config {
 	}
 
 	cfg := Config{}
-	cfg.ServerHost = cast.ToString(getOrDefaultValue("SERVER_HOST", "3.126.186.207"))
+	cfg.ServerHost = cast.ToString(getOrDefaultValue("SERVER_HOST", "18.153.65.158"))
 	cfg.Postgres = Postgres{
-		Host:     cast.ToString(getOrDefaultValue("POSTGRES_HOST", "3.126.186.207")),
+		Host:     cast.ToString(getOrDefaultValue("POSTGRES_HOST", "18.153.65.158")),
 		Port:     cast.ToInt(getOrDefaultValue("POSTGRES_PORT", "5432")),
 		User:     cast.ToString(getOrDefaultValue("POSTGRES_USER", "maxmurjon")),
 		Password: cast.ToString(getOrDefaultValue("POSTGRES_PASSWORD", "max22012004")),
 		DataBase: cast.ToString(getOrDefaultValue("POSTGRES_DATABASE", "comics"))}
 	cfg.Redis = Redis{
-		Host:     cast.ToString(getOrDefaultValue("REDIS_HOST", "3.126.186.207")),
+		Host:     cast.ToString(getOrDefaultValue("REDIS_HOST", "18.153.65.158")),
 		Port:     cast.ToInt(getOrDefaultValue("REDIS_PORT", "5432")),
 		Password: cast.ToString(getOrDefaultValue("REDIS_PASSWORD", "max22012004")),
 		DataBase: cast.ToString(getOrDefaultValue("REDIS_DATABASE", "comics"))}
